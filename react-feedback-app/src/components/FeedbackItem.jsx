7f9055d1e4e88d625a6526700e38d469c0e89429
Jeffrey <jeff.vos@aol.com>
2022-07-04 06:23:52 +0200

    init

---
+++ react-feedback-app/src/components/FeedbackItem.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+
+function FeedbackItem({item}) {
+  return (
+    <Card reverse={true}>
+        <div className='num-display'>{item.rating}</div>
+        <div className='text-display'>{item.text}</div>
+    </Card>
+  )
+}
+
+export default FeedbackItem
