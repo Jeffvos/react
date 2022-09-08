6c29bd209f91b0f670084b47d1246d6301a730f6
Jeffrey <jeff.vos@aol.com>
2022-09-08 10:18:34 +0200

    added loading/spinner

--- react-feedback-app/src/components/FeedbackList.jsx
+++ react-feedback-app/src/components/FeedbackList.jsx
@@ -1,15 +1,16 @@
 import {motion, AnimatePresence} from 'framer-motion'
 import { useContext } from 'react'
 import Feedbackitem from './FeedbackItem'
+import Spinner from './shared/Spinner'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackList(){
-    const {feedback} = useContext(FeedbackContext)
+    const {feedback, isLoading} = useContext(FeedbackContext)
     
-    if(!feedback || feedback.length === 0) {
+    if(!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback Yet</p>
     }
-    return (
+    return isLoading ? <Spinner /> : (
         <div className='feedback-list'>
             <AnimatePresence>
                 {feedback.map((item)=>(
@@ -24,7 +25,8 @@ function FeedbackList(){
                 ))}
             </AnimatePresence>
         </div>
-    )}
+    )
+    }
 //     return (
 //     <div className='feedback-list'>
 //         {feedback.map((item)=>(
